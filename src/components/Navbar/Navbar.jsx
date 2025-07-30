3b51b2925324ef8f4580a258cdc59da2ae40cdd3
kevuxxi <kevinmpz10@icloud.com>
2025-07-30 17:58:18 -0300

    estilo navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,23 +2,35 @@ import { Link } from "react-router-dom";
 import CartWidget from "../CartWidget/CartWidget";
 import './Navbar.css';
 import logo from '../../assets/Gemini_Generated_Image_rj4baqrj4baqrj4b (1).png';
-
+import { useState } from "react";
 
 const Navbar = () => {
-    return (
-        <div className="navbar">
-            <Link to={'/'}> <img src={logo} alt="Mochi House Logo" className="logo" /></Link>
+  const [menuAbierto, setMenuAbierto] = useState(false);
 
-            <ul className="contenedor-links">
-                <Link to={`/category/Alimentos`}> <li className="nav-links">Alimentos</li></Link>
-                <Link to={`/category/Correas`}> <li className="nav-links">Correas</li></Link>
-                <Link to={`/category/Camas`}><li className="nav-links">Camas</li></Link>
-                <Link to={`/category/Otros`}><li className="nav-links">Otros</li></Link>
-            </ul>
+  const toggleMenu = () => {
+    setMenuAbierto(!menuAbierto);
+  };
 
-            <CartWidget />
-        </div>
-    )
-}
+  return (
+    <div className="navbar">
+      <Link to={'/'}>
+        <img src={logo} alt="Mochi House Logo" className="logo" />
+      </Link>
+
+      <button className="menu-toggle" onClick={toggleMenu}>
+        ☰
+      </button>
+
+      <ul className={`contenedor-links ${menuAbierto ? 'abierto' : ''}`}>
+        <Link to={`/category/Alimentos`}><li className="nav-links">Alimentos</li></Link>
+        <Link to={`/category/Correas`}><li className="nav-links">Correas</li></Link>
+        <Link to={`/category/Camas`}><li className="nav-links">Camas</li></Link>
+        <Link to={`/category/Otros`}><li className="nav-links">Otros</li></Link>
+      </ul>
+
+      <CartWidget />
+    </div>
+  );
+};
 
 export default Navbar;
