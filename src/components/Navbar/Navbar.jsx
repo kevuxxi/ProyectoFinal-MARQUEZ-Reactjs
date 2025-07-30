6c45e71bbefcb8219aa309c6eb0729898f3c5525
kevuxxi <kevinmpz10@icloud.com>
2025-07-30 19:00:13 -0300

    hide menu function

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,32 +5,36 @@ import logo from '../../assets/Gemini_Generated_Image_rj4baqrj4baqrj4b (1).png';
 import { useState } from "react";
 
 const Navbar = () => {
-  const [menuAbierto, setMenuAbierto] = useState(false);
+    const [menuAbierto, setMenuAbierto] = useState(false);
 
-  const toggleMenu = () => {
-    setMenuAbierto(!menuAbierto);
-  };
+    const toggleMenu = () => {
+        setMenuAbierto(!menuAbierto);
+    };
 
-  return (
-    <div className="navbar">
-      <Link to={'/'}>
-        <img src={logo} alt="Mochi House Logo" className="logo" />
-      </Link>
+    const hidetogglemenu = () => {
+        setMenuAbierto((false))
+    }
 
-      <button className="menu-toggle" onClick={toggleMenu}>
-        ☰
-      </button>
+    return (
+        <div className="navbar">
+            <Link to={'/'}>
+                <img src={logo} alt="Mochi House Logo" className="logo" />
+            </Link>
 
-      <ul className={`contenedor-links ${menuAbierto ? 'abierto' : ''}`}>
-        <Link to={`/category/Alimentos`}><li className="nav-links">Alimentos</li></Link>
-        <Link to={`/category/Correas`}><li className="nav-links">Correas</li></Link>
-        <Link to={`/category/Camas`}><li className="nav-links">Camas</li></Link>
-        <Link to={`/category/Otros`}><li className="nav-links">Otros</li></Link>
-      </ul>
+            <button className="menu-toggle" onClick={toggleMenu}>
+                ☰
+            </button>
 
-      <CartWidget />
-    </div>
-  );
+            <ul className={`contenedor-links ${menuAbierto ? 'abierto' : ''}`}>
+                <Link to={`/category/Alimentos`} onClick={hidetogglemenu}><li className="nav-links">Alimentos</li></Link>
+                <Link to={`/category/Correas`} onClick={hidetogglemenu}><li className="nav-links">Correas</li></Link>
+                <Link to={`/category/Camas`} onClick={hidetogglemenu}><li className="nav-links">Camas</li></Link>
+                <Link to={`/category/Otros`} onClick={hidetogglemenu}><li className="nav-links">Otros</li></Link>
+            </ul>
+
+            <CartWidget />
+        </div>
+    );
 };
 
 export default Navbar;
